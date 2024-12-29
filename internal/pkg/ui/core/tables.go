138619c0e8322e9872630d4f61a37cacded66b98
Vinesh Bhanderi <vineshb93@gmail.com>
2024-12-29 23:01:51 +0000

    Core: Removed old tables functions

--- internal/pkg/ui/core/tables.go
+++ internal/pkg/ui/core/tables.go
@@ -207,13 +207,13 @@ func (inst *SelectableTable[T]) SetSearchDoneFunc(handler func(key tcell.Key)) {
 	var highlight_search = func(key tcell.Key) {
 		switch key {
 		case tcell.KeyEnter:
-			inst.searchPositions = HighlightTableSearch(
+			inst.searchPositions = highlightTableSearch(
 				inst.Table,
 				inst.GetSearchText(),
 				[]int{},
 			)
 		case tcell.KeyCtrlR:
-			ClearSearchHighlights(inst.Table)
+			clearSearchHighlights(inst.Table)
 			inst.searchPositions = nil
 		}
 		return
@@ -279,76 +279,7 @@ func (inst *DetailsTable) SetData(data []TableRow) {
 	}
 }
 
-func InitSelectableTable(
-	table *tview.Table,
-	title string,
-	headings TableRow,
-	data []TableRow,
-	sortableColumns []int,
-) {
-	table.
-		Clear().
-		SetBorders(false).
-		SetFixed(1, len(headings)-1)
-	table.
-		SetTitle(title).
-		SetTitleAlign(tview.AlignLeft).
-		SetBorderPadding(0, 0, 0, 0).
-		SetBorder(true)
-
-	if len(data) > 0 {
-		if len(headings) != len(data[0]) {
-			log.Panicln("Table data and headings dimensions do not match")
-		}
-	}
-
-	var tableTitle = fmt.Sprintf("%s (%d)", title, len(data))
-	table.SetTitle(tableTitle)
-
-	table.SetSelectable(true, false).SetSelectedStyle(
-		tcell.Style{}.Background(MoreContrastBackgroundColor),
-	)
-
-	for col, heading := range headings {
-		table.SetCell(0, col, tview.NewTableCell(heading).
-			SetAlign(tview.AlignLeft).
-			SetTextColor(SecondaryTextColor).
-			SetSelectable(false).
-			SetBackgroundColor(ContrastBackgroundColor),
-		)
-	}
-
-	for rowIdx, rowData := range data {
-		for colIdx, cellData := range rowData {
-			// the table render process the full string making it extremly slow so
-			// we have to clamp the text length
-			var text = ClampStringLen(&cellData, 180)
-			table.SetCell(rowIdx+1, colIdx, tview.NewTableCell(text).
-				SetReference(cellData).
-				SetAlign(tview.AlignLeft),
-			)
-		}
-	}
-}
-
-func ExtendTable(table *tview.Table, title string, data []TableRow) {
-	var rows = table.GetRowCount()
-	// Don't count the headings row in the title
-	var tableTitle = fmt.Sprintf("%s (%d)", title, len(data)+rows-1)
-	table.SetTitle(tableTitle)
-
-	for rowIdx, rowData := range data {
-		for colIdx, cellData := range rowData {
-			var text = ClampStringLen(&cellData, 180)
-			table.SetCell(rowIdx+rows, colIdx, tview.NewTableCell(text).
-				SetReference(cellData).
-				SetAlign(tview.AlignLeft),
-			)
-		}
-	}
-}
-
-func SearchRefsInTable(table *tview.Table, searchCols []int, search string) []int {
+func searchRefsInTable(table *tview.Table, searchCols []int, search string) []int {
 	var resultPositions = []int{}
 	if len(search) <= 0 {
 		return resultPositions
@@ -377,37 +308,7 @@ func SearchRefsInTable(table *tview.Table, searchCols []int, search string) []in
 	return resultPositions
 }
 
-func InitBasicTable(
-	table *tview.Table, title string, data []TableRow, headingTop bool,
-) {
-	table.
-		Clear().
-		SetBorders(false)
-	table.
-		SetTitle(title).
-		SetTitleAlign(tview.AlignLeft).
-		SetBorderPadding(0, 0, 1, 1).
-		SetBorder(true)
-
-	table.SetSelectable(true, true).SetSelectedStyle(
-		tcell.Style{}.Background(MoreContrastBackgroundColor),
-	)
-
-	for rowIdx, rowData := range data {
-		for colIdx, cellData := range rowData {
-			textColor := TextColour
-			if headingTop && rowIdx > 0 || (!headingTop && colIdx > 0) {
-				textColor = TertiaryTextColor
-			}
-			table.SetCell(rowIdx, colIdx, tview.NewTableCell(cellData).
-				SetAlign(tview.AlignLeft).
-				SetTextColor(textColor),
-			)
-		}
-	}
-}
-
-func ClearSearchHighlights(table *tview.Table) {
+func clearSearchHighlights(table *tview.Table) {
 	var rows = table.GetRowCount()
 	var cols = table.GetColumnCount()
 
@@ -418,16 +319,16 @@ func ClearSearchHighlights(table *tview.Table) {
 	}
 }
 
-func HighlightTableSearch(
+func highlightTableSearch(
 	table *tview.Table,
 	search string,
 	cols []int,
 ) []int {
-	ClearSearchHighlights(table)
+	clearSearchHighlights(table)
 
 	var foundPositions []int
 	if len(search) > 0 {
-		foundPositions = SearchRefsInTable(table, cols, search)
+		foundPositions = searchRefsInTable(table, cols, search)
 		if len(foundPositions) > 0 {
 			table.Select(foundPositions[0], 0)
 		}
