6fca31a116fdb79aa6e1a5cabe9af787c26e4d2b
Vinesh Bhanderi <vineshb93@gmail.com>
2024-12-29 18:04:28 +0000

    Core: Added ServicePageView to replace ServiceView

--- internal/pkg/ui/core/service_view.go
+++ internal/pkg/ui/core/service_view.go
@@ -133,3 +133,48 @@ func (inst *ServiceView) paneResizeHightHandler(
 
 	return event
 }
+
+type ServicePageView struct {
+	*tview.Flex
+	LastFocusedView tview.Primitive
+	selectedViewIdx int
+	app             *tview.Application
+	logger          *log.Logger
+}
+
+func NewServicePageView(
+	app *tview.Application,
+	logger *log.Logger,
+) *ServicePageView {
+	var view = &ServicePageView{
+		Flex:            tview.NewFlex(),
+		LastFocusedView: nil,
+		app:             app,
+		logger:          logger,
+	}
+
+	view.SetDirection(tview.FlexRow)
+
+	return view
+}
+
+func (inst *ServicePageView) InitViewNavigation(orderedViews []View) {
+	var viewIdx = 0
+	var numViews = len(orderedViews)
+	inst.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyCtrlJ:
+			viewIdx = (viewIdx - 1 + numViews) % numViews
+			inst.LastFocusedView = orderedViews[viewIdx]
+			inst.app.SetFocus(inst.LastFocusedView)
+			return nil
+		case tcell.KeyCtrlK:
+			viewIdx = (viewIdx + 1) % numViews
+			inst.LastFocusedView = orderedViews[viewIdx]
+			inst.app.SetFocus(inst.LastFocusedView)
+			return nil
+		}
+
+		return event
+	})
+}
