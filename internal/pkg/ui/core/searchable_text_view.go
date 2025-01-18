6992295fc9bcbe07c034976e4166b011789bedb2
Vinesh Bhanderi <vineshb93@gmail.com>
2025-01-18 22:56:19 +0000

    CoreTextView: Improved search position tracking and selection

--- internal/pkg/ui/core/searchable_text_view.go
+++ internal/pkg/ui/core/searchable_text_view.go
@@ -14,6 +14,7 @@ type SearchableTextView struct {
 	*SearchableView
 	ErrorMessageCallback func(text string, a ...any)
 	textArea             *tview.TextArea
+	lineCount            int
 	searchPositions      [][]int
 	nextSearchPosition   int
 	title                string
@@ -27,48 +28,28 @@ func NewSearchableTextView(title string, app *tview.Application) *SearchableText
 		SearchableView:       searchableView,
 		ErrorMessageCallback: func(text string, a ...any) {},
 		textArea:             textArea,
+		lineCount:            0,
 		searchPositions:      nil,
 		nextSearchPosition:   0,
 		title:                title,
 	}
 
-	view.textArea.SetClipboard(
-		func(s string) { clipboard.WriteAll(s) },
-		func() string {
-			var res, _ = clipboard.ReadAll()
-			return res
-		},
-	).
+	view.textArea.
+		SetClipboard(
+			func(s string) { clipboard.WriteAll(s) },
+			func() string {
+				var res, _ = clipboard.ReadAll()
+				return res
+			},
+		).
 		SetSelectedStyle(
 			tcell.Style{}.Background(MoreContrastBackgroundColor),
 		)
+
 	view.SearchableView.
 		SetTitle(title).
 		SetTitleAlign(tview.AlignLeft).
 		SetBorder(true)
-
-	var updateSearchPosition = func() {
-		if len(view.searchPositions) == 0 {
-			return
-		}
-
-		var pos = view.searchPositions[view.nextSearchPosition]
-		view.SearchableView.SetTitle(fmt.Sprintf(
-			"%s [%s: %d/%d]",
-			view.title,
-			view.GetSearchText(),
-			view.nextSearchPosition+1,
-			len(view.searchPositions),
-		))
-		view.textArea.Select(pos[0], pos[1])
-		// Provide 5 lines of previous text for context if available
-		var selectedLine = countRune(view.GetText(), '\n', pos[0])
-		var contextLineCount = 5
-		if selectedLine >= contextLineCount {
-			selectedLine -= contextLineCount
-		}
-		view.textArea.SetOffset(selectedLine, 0)
-	}
 
 	view.textArea.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
@@ -132,7 +113,7 @@ func NewSearchableTextView(title string, app *tview.Application) *SearchableText
 		}
 
 		if updateSearch {
-			updateSearchPosition()
+			view.updateSearchPosition()
 		}
 
 		return nil
@@ -141,16 +122,17 @@ func NewSearchableTextView(title string, app *tview.Application) *SearchableText
 	view.SearchableView.SetSearchDoneFunc(func(key tcell.Key) {
 		switch key {
 		case APP_KEY_BINDINGS.Done:
+			view.searchPositions = nil
+			view.nextSearchPosition = 0
 			var search = searchableView.GetSearchText()
 			var text = textArea.GetText()
 			if len(search) == 0 || len(text) == 0 {
 				view.SearchableView.SetTitle(view.title)
-				view.searchPositions = nil
 				return
 			}
 			if expr, err := regexp.Compile(search); err == nil {
 				view.searchPositions = expr.FindAllStringIndex(text, -1)
-				updateSearchPosition()
+				view.updateSearchPosition()
 			}
 		}
 	})
@@ -161,7 +143,7 @@ func NewSearchableTextView(title string, app *tview.Application) *SearchableText
 func countRune(s string, r rune, limit int) int {
 	var count = 0
 	for _, c := range s {
-		if limit <= 0 {
+		if limit == 0 {
 			break
 		}
 		limit--
@@ -173,10 +155,42 @@ func countRune(s string, r rune, limit int) int {
 	return count
 }
 
+func (inst *SearchableTextView) middleTextOffset(currentRow int) int {
+	var _, _, _, height = inst.textArea.GetInnerRect()
+	var halfVisible = height / 2
+
+	if inst.lineCount-currentRow <= halfVisible {
+		return max(inst.lineCount-height+1, 0)
+	} else {
+		return max(currentRow-(halfVisible-1), 0)
+	}
+}
+
+func (inst *SearchableTextView) updateSearchPosition() {
+	if len(inst.searchPositions) == 0 {
+		return
+	}
+
+	inst.SearchableView.SetTitle(fmt.Sprintf(
+		"%s [%s: %d/%d]",
+		inst.title,
+		inst.GetSearchText(),
+		inst.nextSearchPosition+1,
+		len(inst.searchPositions),
+	))
+
+	var pos = inst.searchPositions[inst.nextSearchPosition]
+	inst.textArea.Select(pos[0], pos[1])
+	var selectedLine = countRune(inst.GetText(), '\n', pos[0]) - 1
+	var scrollOffset = inst.middleTextOffset(selectedLine)
+	inst.textArea.SetOffset(scrollOffset, 0)
+}
+
 func (inst *SearchableTextView) GetText() string {
 	return inst.textArea.GetText()
 }
 
 func (inst *SearchableTextView) SetText(text string, cursorAtTheEnd bool) {
+	inst.lineCount = countRune(text, '\n', -1)
 	inst.textArea.SetText(text, cursorAtTheEnd)
 }
