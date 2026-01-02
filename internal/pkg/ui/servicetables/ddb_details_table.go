f23e93032e5bc9eae0c966669373bc71a088a8f9
Vinesh Bhanderi <vineshb93@gmail.com>
2026-01-02 22:01:44 +0000

    DDB: Display more table info in the details table

--- internal/pkg/ui/servicetables/ddb_details_table.go
+++ internal/pkg/ui/servicetables/ddb_details_table.go
@@ -48,29 +48,53 @@ func (inst *DynamoDBDetailsTable) populateDetailsTable() {
 	var sortKey = ""
 
 	if inst.data != nil {
-		for _, atter := range inst.data.KeySchema {
-			switch atter.KeyType {
-			case types.KeyTypeHash:
-				partitionKey = aws.ToString(atter.AttributeName)
-			case types.KeyTypeRange:
-				sortKey = aws.ToString(atter.AttributeName)
-			}
+		var deleteProtection = "DISABLED"
+		if aws.ToBool(inst.data.DeletionProtectionEnabled) {
+			deleteProtection = "ENABLED"
 		}
 
-		var gsiList = []core.TableRow{}
-		for _, gsi := range inst.data.GlobalSecondaryIndexes {
-			gsiList = append(gsiList, core.TableRow{"GSI", fmt.Sprintf("%s", aws.ToString(gsi.IndexName))})
+		var billingMode = string(types.BillingModeProvisioned)
+		if billing := inst.data.BillingModeSummary; billing != nil {
+			billingMode = string(billing.BillingMode)
 		}
 
 		tableData = []core.TableRow{
 			{"Name", aws.ToString(inst.data.TableName)},
 			{"Status", fmt.Sprintf("%s", inst.data.TableStatus)},
-			{"CreationDate", inst.data.CreationDateTime.Format(time.DateTime)},
-			{"PartitionKey", partitionKey},
-			{"SortKey", sortKey},
-			{"ItemCount", fmt.Sprintf("%d", aws.ToInt64(inst.data.ItemCount))},
+			{"Creation Date", aws.ToTime(inst.data.CreationDateTime).Format(time.DateTime)},
+			{"Item Count", fmt.Sprintf("%d", aws.ToInt64(inst.data.ItemCount))},
+			{"Billing Mode", billingMode},
+			{"Delete Protection", deleteProtection},
+			{"Arn", aws.ToString(inst.data.TableArn)},
+		}
+
+		var keyData = []core.TableRow{}
+		for _, atter := range inst.data.KeySchema {
+			switch atter.KeyType {
+			case types.KeyTypeHash:
+				partitionKey = aws.ToString(atter.AttributeName)
+				keyData = append(keyData, core.TableRow{"Partition Key", partitionKey})
+			case types.KeyTypeRange:
+				sortKey = aws.ToString(atter.AttributeName)
+				keyData = append(keyData, core.TableRow{"Sort Key", sortKey})
+			}
+		}
+		tableData = append(tableData, keyData...)
+
+		var gsiList = []core.TableRow{}
+		for _, gsi := range inst.data.GlobalSecondaryIndexes {
+			gsiList = append(gsiList, core.TableRow{"GSI", fmt.Sprintf("%s", aws.ToString(gsi.IndexName))})
 		}
 		tableData = append(tableData, gsiList...)
+
+		if sse := inst.data.SSEDescription; sse != nil {
+			var sseData = []core.TableRow{
+				{"Encryption Type", string(sse.SSEType)},
+				{"Encryption Status", string(sse.Status)},
+				{"KMS Key Arn", aws.ToString(sse.KMSMasterKeyArn)},
+			}
+			tableData = append(tableData, sseData...)
+		}
 	}
 
 	inst.SetData(tableData)
